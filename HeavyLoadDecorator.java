d000d406df950d917b24883e890e5e3630ca9850
TDrones <flh56@txstate.edu>
2026-05-13 12:09:00 -0500

    Pushed HeavyLoadDecorator

---
+++ HeavyLoadDecorator.java
@@ -0,0 +1,11 @@
+public class HeavyLoadDecorator extends ShippingDecorator {
+
+    public HeavyLoadDecorator(ShippingStrategy wrappedStrategy) {
+        super(wrappedStrategy);
+    }
+
+    @Override
+    public double calculate(double weight, double distance) {
+        return super.calculate(weight, distance) + 15.0;
+    }
+}
