ccbf34e62594cb3bded831b6316b9efe56512914
tduccy <thomas.tran7678@gmail.com>
2026-05-13 11:15:50 -0500

    partb

---
+++ solution/ShippingPolicyFactory.java
@@ -0,0 +1,27 @@
+public class ShippingPolicyFactory {
+
+    public static ShippingStrategy getStrategy(String type, double weight) {
+
+        ShippingStrategy strategy;
+
+        if (type.equals("Flat Rate")) {
+            strategy = new FlatRateStrategy();
+
+        } else if (type.equals("Weight-Based")) {
+            strategy = new WeightBasedStrategy();
+
+        } else if (type.equals("Distance-Based")) {
+            strategy = new DistanceBasedStrategy();
+
+        } else {
+            strategy = new CarrierSpecificStrategy();
+        }
+
+        // Decorator Pattern
+        if (weight > 20) {
+            strategy = new HeavyLoadDecorator(strategy);
+        }
+
+        return strategy;
+    }
+}
