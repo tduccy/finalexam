98c68a1ed05414c333b465c0d43cfd4458346680
goosebutters <acbusceme@gmail.com>
2026-05-13 11:50:58 -0500

    added import

--- solution/partb/SimpleShippingApp.java
+++ solution/partb/SimpleShippingApp.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import parta.ShippingStrategy;
 
 public class SimpleShippingApp extends JFrame {
 
